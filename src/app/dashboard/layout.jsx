c1a8d74b1652fece61d61ed51a61584adecbffd3
Suphawida <66022983@up.ac.th>
2026-01-24 09:00:26 +0700

    layout

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,8 +1,15 @@
-"use client";
-
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import Sidebar from "../../../components/Sidebar";
 
-export default function DashboardLayout({ children }) {
+export default async function DashboardLayout({ children }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <Sidebar>
       {children}
